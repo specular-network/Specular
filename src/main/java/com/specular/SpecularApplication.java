3afd8eb7a5cca25349ddd82b6b8f273b8b92123b
crtrpt <admin@crtrpt.com>
2019-03-01 11:08:56 +0800

    'init'

---
+++ src/main/java/com/specular/SpecularApplication.java
@@ -0,0 +1,13 @@
+package com.specular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecularApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SpecularApplication.class, args);
+    }
+    
+}
